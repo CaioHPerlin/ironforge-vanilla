24d9e33a54c00d4b90ca221946a06b644f93479e
CaioHPerlin <caiohperlin@gmail.com>
2025-01-24 04:28:24 -0400

    chore: setup tailwindcss

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  root: './src',
+  publicDir: '../public/',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
 });
